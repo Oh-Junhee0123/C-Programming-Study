c9b7381483540ae1ca2d2ae76eab0cf2a8ff6ba7
Junhee Oh <63595478+junhee16177@users.noreply.github.com>
2020-06-24 22:58:12 +0900

    Create HelloComment.c
    HelloComment.c
    
    Hello.c에 주석을 적성한다.

---
+++ Part01/Chapter02/HelloComment.c
@@ -0,0 +1,20 @@
+/*
+제목 : Hello world 출력하기
+기능 : 문자열의 출력
+파일 이름 : HelloComment.c
+날짜 : 2020. 06. 24.
+작성자 : 오준희
+*/
+
+#include <stdio.h>
+
+int main(void) //main 함수의 시작
+{
+    /*
+    이 함수 내에서는 하나의 문자열을 출력한다.
+    문자열은 모니터로 출력된다.
+    */
+    
+    printf("Hello world! \n"); //문자열의 출력
+    return 0; //0의 반환
+} //main 함수의 끝

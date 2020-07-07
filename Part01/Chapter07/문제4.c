206b807e9fb82514ac0ed72d16ba73dc7b7cea0a
Oh-Junhee0123 <67366952+Oh-Junhee0123@users.noreply.github.com>
2020-07-08 03:30:12 +0900

    Create 문제4.c
    문제4.c
    
    프로그램 사용자로부터 입력 받은 숫자에 해당하는 구구단을 출력하되 역순으로 출력하는 프로그램을 작성한다.

---
+++ Part01/Chapter07/문제4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  int i = 9;
+  
+  printf("역순으로 출력할 단 입력 : ");
+  scanf("%d", &num);
+  
+  while(i > 0)
+  {
+    printf("%d×%d=%d \n", num, i, num*i);
+    i--;
+  }
+  return 0;
+}

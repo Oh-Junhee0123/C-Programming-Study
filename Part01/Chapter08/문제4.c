e5d45fcec3a4bc3ae8186c8a954ce937d900c836
Oh-Junhee0123 <67366952+Oh-Junhee0123@users.noreply.github.com>
2020-07-10 00:52:04 +0900

    Create 문제4.c

---
+++ Part01/Chapter08/문제4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1, num2;
+  int result;
+  printf("두 개의 정수 입력 : ");
+  scanf("%d %d", &num1, &num2);
+  
+  result = (num1 > num2) ? num2 - num1 : num2 - num1;
+  printf("뺄셈 결과 : %d \n", result);
+  return 0;
+}

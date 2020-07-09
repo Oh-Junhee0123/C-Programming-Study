6e9fc9302c9e67c3c8b68374827400978ddc16de
Oh-Junhee0123 <67366952+Oh-Junhee0123@users.noreply.github.com>
2020-07-10 02:23:42 +0900

    Create AbsoCompare.c

---
+++ Part01/Chapter09/AbsoCompare.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int AbsoCompare(int num1, int num2);
+int GetAbsoValue(int num);
+
+int main()
+{
+  int num1, num2;
+  printf("두 개의 정수 입력 : ");
+  scanf("%d %d", &num1, &num2);
+  printf("%d와 %d중 절댓값이 큰 정수 : %d \n", num1, num2, AbsoCompare(num2, num3));
+  return 0;
+}
+
+int Absocompare(int num1, int num2)
+{
+  if(GetAbsoValue(num1) > GetAbsoValue(num2))
+    return num1;
+  else 
+    return num2;
+}
+
+int GetAbsoValue(int num)
+{
+  if(num < 0)
+    return num * (-1);
+  else
+    return num;
+}
